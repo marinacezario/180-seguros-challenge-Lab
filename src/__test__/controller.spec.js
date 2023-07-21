4b0fb6758fb25b0fc2fc766787e8e3079a0fb92b
marinacezario <marina.cezario@gmail.com>
2023-07-20 21:33:21 -0300

    test: create test for controller.js

--- src/__test__/controller.spec.js
+++ src/__test__/controller.spec.js
@@ -1,30 +1,88 @@
-const validator = require('express-validator');
-const express = require('express');
-const { app } = require('../app');
-const { getPolicyholderById } = require('../../src/controller');
+const { getAllPolicyholders, getPolicyholderById, createPolicyholder } = require('../controller');
+const { findAllPolicyholders, findPolicyholderById, insertPolicyholder } = require('../db.js');
 
-jest.mock('express-validator', () => ({
-  matchedData: jest.fn(),
-  checkSchema: jest.fn()
+jest.mock('../db.js', () => ({
+  findAllPolicyholders: jest.fn(),
+  findPolicyholderById: jest.fn(),
+  insertPolicyholder: jest.fn()
 }));
 
-jest.mock('express', () => ({
-    post: jest.fn()
-}))
+test('getAllPolicyholders should return all policyholders', () => {
+  const mockRequest = {};
+  const mockResponse = {
+    json: jest.fn(),
+  };
+  findAllPolicyholders.mockReturnValue([
+    { id: 325, name: 'Vanessa' },
+    { id: 201, name: 'Marina' },
+  ]);
+  getAllPolicyholders(mockRequest, mockResponse);
+  expect(mockResponse.json).toHaveBeenCalledWith([
+    { id: 325, name: 'Vanessa' },
+    { id: 201, name: 'Marina' },
+  ]);
+});
 
-describe('testes para a rota de busca de segurados por ID', () => {
-    
-    it('Deve retornar um segurado existente quando um ID válido é fornecido', () => {
-        const segurado = {
-            id: 475,
-            name: "Aline"
-        };
-        const response = getPolicyholderById(app).get(`/policyholders/${segurado.id}`);
+// mostra apenas o id informado
+test('getPolicyholderById should return the selected policyholder', () => {
+  const mockRequestValid = {
+    params: { policyholderId: 325 },
+  };
+  const mockResponse = {
+    json: jest.fn(),
+    status: jest.fn(),
+  };
+  findPolicyholderById.mockReturnValue([
+    { id: 325, name: 'Vanessa' },
+  ]);
+  getPolicyholderById(mockRequestValid, mockResponse);
+  expect(mockResponse.json).toHaveBeenCalledWith([
+    { id: 325, name: 'Vanessa' },
+  ]);
+});
 
-        expect(response.status).toBe(200);
-        expect(response.body).toEqual({
-            id: segurado.id,
-            name: segurado.name
-        })
-    })
-})
+// teste de criação
+  test('createPolicyholder should return 400 when an empty request is provided', () => {
+    const mockRequestEmpty = {};
+    const mockResponse = {
+      status: jest.fn(() => mockResponse),
+      json: jest.fn(),
+    };
+    insertPolicyholder.mockReturnValue(null);
+    createPolicyholder(mockRequestEmpty, mockResponse);
+    expect(mockResponse.status).toHaveBeenCalledWith(400);
+  });
+
+  test('getPolicyholderById should return 404 if policyholder is not found', () => {
+    const mockRequestInvalid = {
+      params: { policyholderId: 999 },
+    };
+    const mockResponse = {
+      json: jest.fn(),
+      status: jest.fn(() => mockResponse),
+    };
+    findPolicyholderById.mockReturnValue(null);
+
+    getPolicyholderById(mockRequestInvalid, mockResponse);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(404);
+    expect(mockResponse.json).toHaveBeenCalled();
+  });
+
+  test('createPolicyholder should create a new policyholder', () => {
+    const mockRequest = {
+      id: 110,
+      name: 'Keila'
+    };
+    const mockResponse = {
+      status: jest.fn(() => mockResponse),
+      json: jest.fn(),
+    };
+    insertPolicyholder.mockReturnValue([
+      { id: 110, name: 'Keila' }
+    ]);
+    createPolicyholder(mockRequest, mockResponse);
+    expect(mockResponse.json).toHaveBeenCalledWith([
+      { id: 110, name: 'Keila' }
+    ]);
+  });
